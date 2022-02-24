c612a49aaee1a7147480ce49696e9cd1d29b7624
simon <simonaertsbecode@gmail.com>
2022-02-24 16:36:53 +0100

    working on modal

--- src/components/modals/AnmneseModal.tsx
+++ src/components/modals/AnmneseModal.tsx
@@ -1,6 +1,9 @@
 import { ReactNode } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
+//** react icons */
+import { ImCross } from 'react-icons/im';
+
 const backdropVariant = {
    visible: {
       opacity: 1,
@@ -10,13 +13,30 @@ const backdropVariant = {
    },
 };
 
+const modal = {
+   hidden: {
+      y: '-100vh',
+      opacity: 0,
+   },
+   visible: {
+      y: '30%',
+      opacity: 1,
+      transition: { delay: 0.5 },
+   },
+};
+
 const AnmneseModal = ({ showModal, ...props }: { showModal: boolean; children: ReactNode }) => {
-   console.log(showModal);
    return (
       <AnimatePresence exitBeforeEnter>
          {showModal && (
-            <motion.section className='backdrop' variants={backdropVariant} animate='visible' initial='hidden'>
-               <section className='modal'>{props.children}</section>
+            <motion.section className='backdrop' variants={backdropVariant} animate='visible' initial='hidden' exit='hidden'>
+               <motion.section className='modal' variants={modal}>
+                  <button>
+                     <ImCross />
+                  </button>
+
+                  {props.children}
+               </motion.section>
             </motion.section>
          )}
       </AnimatePresence>
