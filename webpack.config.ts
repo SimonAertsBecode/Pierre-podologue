5d7d8ac40a760ecdd9670109ebfebffbd848fdeb
simon <simonaertsbecode@gmail.com>
2022-03-09 14:18:57 +0100

    working on webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,3 +1,28 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
 module.exports = {
-    entry: "./src/index.tsx"
-}
+   entry: './src/index.tsx',
+   module: {
+      rules: [
+         {
+            test: /\.svg$/,
+            use: 'svg-inline-loader',
+         },
+         {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+         },
+         {
+            test: /\.(js)$/,
+            use: 'babel-loader',
+         },
+      ],
+   },
+   output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'bundle.js',
+   },
+   plugins: [new HtmlWebpackPlugin()],
+   mode: process.env.NODE_ENV === "production" ? "production" : "development"
+};
